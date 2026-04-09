6aa802011ba8363f2e6b1ccfebcc8994ce6c0743
pradyneshnarke <146545031+pradyneshnarke@users.noreply.github.com>
2026-04-09 17:52:20 +0530

    Fixed import of refactored UserHashed model

--- auth/core.py
+++ auth/core.py
@@ -1,6 +1,5 @@
 import os
 from datetime import timedelta, datetime
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError

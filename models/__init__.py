54b514c9ebcbe5ab532a16cb38b51313b4835fda
narke-pradynesh <npradynesh@gmail.com>
2026-04-22 21:22:47 +0530

    Added 'UserDB' to __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .prompt import Prompt
 from .response import Response
-from .user import User, UserHashed, UserSignup
+from .user import User, UserHashed, UserSignup, UserDB
 from .token import Token, TokenData
 
-__all__ = ['Prompt', 'Response',  'User', 'UserHashed', 'Token', 'TokenData','UserSignup']
+__all__ = ['Prompt', 'Response',  'User', 'UserHashed', 'Token', 'TokenData','UserSignup', 'UserDB']

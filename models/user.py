b90143dfaf5c6f42541033740e4c85f297717dcb
narke-pradynesh <npradynesh@gmail.com>
2026-04-22 21:15:02 +0530

    Created UserDB model for storing User in DB

--- models/user.py
+++ models/user.py
@@ -1,13 +1,40 @@
-from pydantic import BaseModel
+from typing import Any
+
+from pydantic import EmailStr, BaseModel
+from sqlmodel import Session, Field, SQLModel
+"""
+    Keep Pydantic models seperate from SQLModel models
+"""
 
 class User(BaseModel):
-    username: str = None
-    email: str = None
-    full_name: str = None
-    disabled: bool = None
+    """
+        Basic pydantic model for User Object Validation
+        Safe to return in the response to client
+    """
+    username: str 
+    email: str 
+    full_name: str 
+    disabled: bool 
+
 
 class UserHashed(User):
-    hashed_password: str = None
+    hashed_password: str 
 
 class UserSignup(User):
-    password: str
+    """
+        Temporary model to store User before hashing
+        Never return to client
+    """
+    password: str 
+
+class UserDB(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username : str = Field(index=True)
+    email : EmailStr = Field(index=True)
+    full_name : str | None = None
+    hashed_password : str = Field(nullable=False)
+
+    def __init__(self, user_hash : UserHashed):
+        self.hashed_password = user_hash.hashed_password
+
+
